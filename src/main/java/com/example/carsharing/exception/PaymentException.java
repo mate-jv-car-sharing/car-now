09e8674abc0de67466386b67b9936191b50f52d1
Kukharenko Yurii <46027349+yurakukh@users.noreply.github.com>
2026-02-15 21:12:59 +0200

    [FIX]: Removed secure data from pushing to repo

--- src/main/java/com/example/carsharing/exception/PaymentException.java
+++ src/main/java/com/example/carsharing/exception/PaymentException.java
@@ -4,6 +4,7 @@ public class PaymentException extends RuntimeException {
     public PaymentException(String message) {
         super(message);
     }
+
     public PaymentException(String message, Throwable cause) {
         super(message, cause);
     }
